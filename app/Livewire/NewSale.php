392938fd7f74bea289eda00f8400377be577b80d
kayehol <kaye.holanda@gmail.com>
2023-10-18 15:57:34 -0300

    wip: add client and product to sale

--- app/Livewire/NewSale.php
+++ app/Livewire/NewSale.php
@@ -8,14 +8,46 @@ use App\Models\Produto;
 
 class NewSale extends Component
 {
-    public string $selectedClient = "";
-    private array $products = [];
+    public string $selectedClientId;
+    public ?Cliente $client;
+    public string $selectedProductId = '';
+    public array $selectedProducts = [];
+    public float $subtotal = 0.0;
+    public float $total = 0.0;
 
     public function render()
     {
         return view('livewire.new-sale')->with([
             'clients' => Cliente::all(),
-            'products' => Produto::all()
+            'products' => Produto::all(),
         ]);
+    }
+
+    public function updating($property, $value)
+    {
+        try {
+            if ($property === 'selectedClientId') {
+                $this->client = Cliente::where('id', $value)->first();
+            }
+        } catch (\Exception $e) {
+            var_dump($e->getMessage());
+            abort(500);
+        }
+    }
+
+    public function add()
+    {
+        try {
+            $product = Produto::where('id', $this->selectedProductId)->first();
+
+            if (!$product) throw new \Exception('No product!');
+            array_push($this->selectedProducts, $product);
+            $this->subtotal += $product->preco;
+            $this->total += $product->preco;
+        } catch (\Exception $e) {
+            dd($e);
+            var_dump($e->getMessage());
+            abort(500);
+        }
     }
 }
--- resources/views/livewire/new-sale.blade.php
+++ resources/views/livewire/new-sale.blade.php
@@ -2,25 +2,69 @@
     <div class="flex flex-row">
         <div>
             <p>Clientes:</p>
-            <select wire:model.live="selectedClient">
+            <select wire:model.live="selectedClientId">
                 @foreach($clients as $client)
-                <option>{{ $client->nome }}</option>
+                <option value="{{ $client->id }}">{{ $client->nome }}</option>
                 @endforeach
             </select>
         </div>
         <div>
             <p>Produtos</p>
-            <select>
+            <select wire:model.live="selectedProductId">
                 @foreach($products as $product)
-                <option value="{{ $product->id }}">{{ $product->descricao }}</option>
+                <option value="{{ $product->id }}">{{ $product->descricao }} - {{ $product->gradacao }}</option>
                 @endforeach
             </select>
-            <button>Adicionar</button>
+            <button wire:click="add">Adicionar</button>
         </div>
     </div>
     <hr>
     <div>
-        <p>Cliente: {{ $selectedClient }}</p>
-        <p>Produtos: </p>
+        @isset($this->client)
+        <div>
+            <p>Cliente: {{ $this->client->nome }}</p>
+            @if($this->client->cnpj)
+            <p>CNPJ: {{ $this->client->cnpj}}</p>
+            @endif
+            @if($this->client->cpf)
+            <p>CPF: {{ $this->client->cpf}}</p>
+            @endif
+            <p>Endereço: {{ $this->client->endereco }}</p>
+            <p>Cidade: {{ $this->client->cidade }}</p>
+            <p>Estado: {{ $this->client->estado}}</p>
+            <p>CEP: {{ $this->client->cep}}</p>
+            <p>Fone: {{ $this->client->fone}}</p>
+            <hr>
+        </div>
+        @endisset
+        @isset($this->selectedProducts)
+        <div>
+            <p>Produtos: </p>
+            @foreach($this->selectedProducts as $selectedProduct)
+            <div>
+                <p>{{ $selectedProduct->descricao }} - {{ $selectedProduct->gradacao }}</p>
+                <p>Valor unitário - {{ $selectedProduct->preco}}</p>
+                <div>
+                    <label for="qtd">Quantidade</label>
+                    <input />
+
+                </div>
+                <button>Remover</button>
+            </div>
+            @endforeach
+            <hr>
+        </div>
+        @endisset
+        @isset($this->subtotal)
+        <div>
+            <p>Subtotal: {{ $this->subtotal }}</p>
+            <hr>
+        </div>
+        @endisset
+        @isset($this->total)
+        <div>
+            <p>Total: {{ $this->total }}</p>
+        </div>
+        @endisset
     </div>
 </div>
